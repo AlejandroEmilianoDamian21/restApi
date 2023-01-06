f61be66a43e8717daf876e2e71c3dcc13932514b
Emiliano Alejandro <Lautarooyt837@gmail.com>
2023-01-06 11:09:12 -0300

    Configuracion del proyecto

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-//console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programacion del mundo");
+import express from 'express'
 
-let lista = ["Emiliano", 21 , true , new Date("Junio 19, 2001 10:24:00") , 
-{
-    "titulo": "Harry Poter" ,
-    "autor":"J. K. Rowling",
-    "fecha": new Date("2001-11-29"),
-    "URL": new URL('http://www.cpte.gob.mx/desarrollo_educativo/pdf/pedagogia/libros_digitales/Rowling,%20Joanne%20K.-%20%201.%20Harry%20Potter%20y%20la%20piedra%20filosofal.doc.pdf')
-}];
+const app = express();
+const port = 3306;
 
-console.log(lista);
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`);
+})
