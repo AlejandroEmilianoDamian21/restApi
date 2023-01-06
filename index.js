ac5d191c87a9c60a794326d78e2c48b628614c68
Emiliano Alejandro <alejandroemiliano21@gmail.com>
2023-01-06 11:43:49 -0300

    Endpoints principales

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ import express from 'express'
 const app = express();
 const port = 3306;
 
+
+app.get('/employes', (req,res)=>{
+    res.send(`Obteninedo empleados`)
+})
+
+
+app.post('/employes', (req,res)=>{
+    res.send(`Creando empleados`)
+})
+
+
+app.put('/employes', (req,res)=>{
+    res.send(`Actualizando de empleados`)
+})
+
+
+app.delete('/employes', (req,res)=>{
+    res.send(`Eliminacion de empleados`)
+})
+
+
+
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
 })
